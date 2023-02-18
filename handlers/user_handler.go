984f24793da42ccddfabd03acc43534f87083bbf
hibakun <hiba.kudus@gmail.com>
2023-02-18 10:50:31 +0700

    feat: [HF] registration cookie

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"github.com/go-playground/validator/v10"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/hibakun/nova-store/config"
 	"github.com/hibakun/nova-store/database"
 	"github.com/hibakun/nova-store/models"
 	"github.com/hibakun/nova-store/utils"
-	"log"
 	"net/http"
+	"time"
 )
 
 func CreateUser(c *fiber.Ctx) error {
@@ -30,7 +32,6 @@ func CreateUser(c *fiber.Ctx) error {
 	}
 
 	if _, err := utils.GetUserByEmail(user.Email); err != false {
-		log.Println(err)
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"status":  "error",
 			"message": "email already exist",
@@ -64,9 +65,32 @@ func CreateUser(c *fiber.Ctx) error {
 		})
 	}
 
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"id":       user.ID,
+		"identity": user.Email,
+		"exp":      time.Now().Add(time.Hour * 24 * 7).Unix(),
+	})
+
+	token, err := t.SignedString([]byte(config.Config("SECRET")))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"status":  "error",
+			"message": "failed to create token",
+		})
+	}
+
+	c.Cookie(&fiber.Cookie{
+		Name:     "USER_SESSION",
+		Value:    token,
+		MaxAge:   3600 * 24 * 7,
+		Expires:  time.Now().Add(time.Hour * 24 * 7),
+		SameSite: "lax",
+	})
+
 	return c.JSON(fiber.Map{
 		"status":  "success",
 		"message": "user created",
 		"data":    user,
+		"token":   token,
 	})
 }
