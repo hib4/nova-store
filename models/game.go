36b6eb9adc80c302af059114e2b4c818a9fd488f
hibakun <hiba.kudus@gmail.com>
2023-02-26 11:09:54 +0700

    fix: [HF] game model

--- models/game.go
+++ models/game.go
@@ -6,19 +6,19 @@ type Game struct {
 	Developer string          `json:"developer" form:"developer" gorm:"not null" validate:"required"`
 	Publisher string          `json:"publisher" form:"publisher" gorm:"not null" validate:"required"`
 	GenreID   []uint          `json:"genre_id" form:"genre_id" gorm:"-" validate:"required"`
-	Genres    []GenreResponse `json:"-" gorm:"foreignKey:ID"`
+	Genres    []GenreResponse `json:"-" gorm:"many2many:game_genres;foreignKey:ID;joinForeignKey:GameID;References:ID;joinReferences:GenreID"`
 	Platform  string          `json:"platform" form:"platform" gorm:"not null" validate:"required"`
 	Image     string          `json:"image" form:"image" gorm:"not null"`
 }
 
 type GameResponse struct {
-	ID        uint            `json:"id" form:"id"`
-	Name      string          `json:"name" form:"name"`
-	Developer string          `json:"developer" form:"developer"`
-	Publisher string          `json:"publisher" form:"publisher"`
-	Genres    []GenreResponse `json:"genres" form:"genres" gorm:"foreignKey:ID"`
-	Platform  string          `json:"platform" form:"platform"`
-	Image     string          `json:"image" form:"image"`
+	ID        uint            `json:"id"`
+	Name      string          `json:"name"`
+	Developer string          `json:"developer"`
+	Publisher string          `json:"publisher"`
+	Genres    []GenreResponse `json:"genres" gorm:"many2many:game_genres;foreignKey:ID;joinForeignKey:GameID;References:ID;joinReferences:GenreID"`
+	Platform  string          `json:"platform"`
+	Image     string          `json:"image"`
 }
 
 func (GameResponse) TableName() string {
--- models/genre.go
+++ models/genre.go
@@ -2,8 +2,7 @@ package models
 
 type Genre struct {
 	Model
-	Name  string         `json:"name" form:"name" gorm:"not null" validate:"required"`
-	Games []GameResponse `json:"-" gorm:"many2many:game_genres;ForeignKey:ID;joinForeignKey:GenreID;References:ID;joinReferences:GameID"`
+	Name string `json:"name" form:"name" gorm:"not null" validate:"required"`
 }
 
 type GenreResponse struct {
