d879c8cb8a339a803f822bdcc58e10291fa81e71
hibakun <hiba.kudus@gmail.com>
2023-03-05 19:50:59 +0700

    fix: [HF] remove godotenv

--- config/config.go
+++ config/config.go
@@ -1,16 +1,9 @@
 package config
 
 import (
-	"github.com/joho/godotenv"
-	"log"
 	"os"
 )
 
 func Config(key string) string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("failed loading .env")
-	}
-
 	return os.Getenv(key)
 }
